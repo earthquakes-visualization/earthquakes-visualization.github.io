93f1419c938b6d52786ad9cf51fa825ba8255029
Daniel Scheffknecht <daniel.scheffkn@gmail.com>
2016-12-21 16:59:25 +0100

    fix: add css to copy

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var browserSync = require('browser-sync').create();
 gulp.task('default', ['babel', 'copy']);
 
 gulp.task('copy', function () {
-  return gulp.src('src/*.html')
+  return gulp.src('src/*.{html,css}')
     .pipe(gulp.dest('./dist/'))
 });
 
