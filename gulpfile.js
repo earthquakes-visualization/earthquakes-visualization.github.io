a5bdab0fd61be4fd0cff58a3ce15e0fe2e7b316f
Daniel Scheffknecht <daniel.scheffkn@gmail.com>
2016-12-21 16:52:58 +0100

    feat: gulp watch and babel

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var babel = require('gulp-babel');
+var browserSync = require('browser-sync').create();
+
+gulp.task('default', ['babel', 'copy']);
+
+gulp.task('copy', function () {
+  return gulp.src('src/*.html')
+    .pipe(gulp.dest('./dist/'))
+});
+
+gulp.task('babel', function () {
+  return gulp.src('src/app.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('./dist/'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('watch', ['browser-sync'], function () {
+  gulp.watch('./src/*.js', ['babel']).on('change', function () {
+    browserSync.reload()
+  });
+  gulp.watch('./src/*.html', ['copy']).on('change', function () {
+    browserSync.reload()
+  });
+})
+
+gulp.task('browser-sync', function () {
+  browserSync.init({
+    server: {
+      baseDir: "./dist/"
+    }
+  })
+});
